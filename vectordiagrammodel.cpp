53a780018121a3a766a2a75eae9ace392cb76522
Aratimaru <Jeka3570@gmail.com>
2023-01-22 12:44:48 +0200

    Add model

--- vectordiagrammodel.cpp
+++ vectordiagrammodel.cpp
@@ -7,21 +7,60 @@ VectorDiagramModel::VectorDiagramModel(QObject *parent)
 }
 
 int VectorDiagramModel::rowCount(const QModelIndex & /*parent*/) const
-{
-   return 2;
+{return _instances.size();
 }
 
 int VectorDiagramModel::columnCount(const QModelIndex & /*parent*/) const
 {
-    return 3;
+    return 3;//
 }
 
 QVariant VectorDiagramModel::data(const QModelIndex &index, int role) const
 {
-    if (role == Qt::DisplayRole)
-       return QString("Row%1, Column%2")
-                   .arg(index.row() + 1)
-                   .arg(index.column() +1);
+    //! \todo add the sanity check
 
-    return QVariant();
+    const TableOfPhases *holderStruct {&_instances[index.row()]};
+
+    switch (index.column()) {
+    case 0:
+        return QVariant::fromValue(holderStruct->phaseA);
+    case 1:
+        return QVariant::fromValue(holderStruct->phaseB);
+    case 2:
+        return QVariant::fromValue(holderStruct->phaseC);
+    default:
+        Q_ASSERT(false);
+        break;
+    }
+
+}
+
+bool VectorDiagramModel::setData(const QModelIndex &index, const QVariant &value, int role)
+{
+    if (index.column() >= columnCount() || index.row() >= _instances.size() )
+        return false;
+
+    if (!value.canConvert<QPair<double, double>>())
+        return false; // conversion may not succeed
+
+    //! \todo intermediary calculations
+
+    auto dataPair = value.value<QPair<double, double>>();
+    TableOfPhases *holderStruct;
+    holderStruct = &_instances[index.row()];
+
+    switch (index.column()) {
+    case 0:
+        holderStruct->phaseA = dataPair;
+        break;
+    case 1:
+        holderStruct->phaseB = dataPair;
+        break;
+    case 2:
+        holderStruct->phaseC= dataPair;
+        break;
+    default:
+        Q_ASSERT(false);
+        break;
+    }
 }
--- vectordiagrammodel.h
+++ vectordiagrammodel.h
@@ -1,8 +1,18 @@
 #ifndef VECTORDIAGRAMMODEL_H
 #define VECTORDIAGRAMMODEL_H
 
-#include <QAbstractTableModel>
 #include "phasevector.h"
+
+#include <QAbstractTableModel>
+
+#define ROWS 2
+#define COLUMNS 3
+
+struct TableOfPhases{
+    QPair<double, double> phaseA{};
+    QPair<double, double> phaseB{};
+    QPair<double, double> phaseC{};
+};
 
 class VectorDiagramModel : public QAbstractTableModel
 {
@@ -12,6 +22,15 @@ public:
     int rowCount(const QModelIndex &parent = QModelIndex()) const override;
     int columnCount(const QModelIndex &parent = QModelIndex()) const override;
     QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    bool setData(const QModelIndex &index, const QVariant &value, int role = Qt::EditRole) override;
+
+signals:
+    void editCompleted(const QString &);
+
+private:
+    QString m_gridData[COLUMNS][ROWS];  //holds text entered into QTableView
+
+    QVector<TableOfPhases> _instances;
 };
 
 #endif // VECTORDIAGRAMMODEL_H
